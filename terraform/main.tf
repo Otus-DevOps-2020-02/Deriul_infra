2ae7b178e2e53248bd606a73c84640480a2a720e
Ruslan Akchurin <akchurin.ruslan@gmail.com>
2020-03-27 09:19:48 -0400

    prepare remote

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 # init
-#terraform {
-  #required_version = "0.12.24"
-#}
+terraform {
+  required_version ~> "0.12"
+}
 
 provider "google" {
-  #version = "2.15"
+  version = "2.15"
   project = var.project
   region  = var.region
 }
@@ -25,15 +25,6 @@ resource "google_compute_firewall" "firewall_puma" {
   }
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["reddit-app"]
-}
-
-# 9292 healthcheck
-resource "google_compute_http_health_check" "tcp-9292-check" {
-  name               = "tcp-9292-check"
-  request_path       = "/"
-  port               = 9292
-  check_interval_sec = 10
-  timeout_sec        = 3
 }
 
 # lets create instances
@@ -74,25 +65,4 @@ resource "google_compute_instance" "app" {
   }
 }
 
-# and put them into the pool
-resource "google_compute_target_pool" "nlb-target-pool" {
-  name             = "nlb-target-pool"
-  session_affinity = "NONE"
-  region           = var.region
 
-  instances = google_compute_instance.app.*.self_link
-
-  health_checks = [
-    google_compute_http_health_check.tcp-9292-check.name
-  ]
-}
-
-# create forwarding rule
-resource "google_compute_forwarding_rule" "network-load-balancer" {
-  name                  = "nlb-test"
-  region                = var.region
-  target                = google_compute_target_pool.nlb-target-pool.self_link
-  port_range            = "9292"
-  ip_protocol           = "TCP"
-  load_balancing_scheme = "EXTERNAL"
-}
